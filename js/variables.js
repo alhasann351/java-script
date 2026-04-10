797314a069a7cf6d4369a07f8f193affb53b66d4
Al-Hasan <alhasann352@gmail.com>
2026-04-10 22:03:29 +0600

    variables

---
+++ js/variables.js
@@ -0,0 +1,16 @@
+const example1 = document.getElementById("example1");
+const example2 = document.getElementById("example2");
+const example3 = document.getElementById("example3");
+
+//example 1
+const myName = "Hasan";
+example1.textContent = `My name is ${myName}`;
+
+//example 2
+let age = 18;
+age = 26;
+example2.textContent = `I am ${age} years old`
+
+//example 3
+let num = 1;
+example3.textContent = `This is ${num}`;
