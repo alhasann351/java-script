6c418b3f923715c95c7f662cce20de79f02a050b
Al-Hasan <alhasann352@gmail.com>
2026-04-11 14:43:26 +0600

    function

---
+++ js/function.js
@@ -0,0 +1,17 @@
+console.log('Function Example');
+
+function sleep() {
+    console.log('This is function');
+}
+sleep();
+
+function nameAge(name, age) {
+console.log(`My name is ${name}, I am ${age} years old`)
+}
+nameAge('Al Hasan', '25');
+
+let result = sum(4, 5);
+console.log(result);
+function sum(a, b) {
+    return a * b;
+}
